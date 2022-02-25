83434eb81357f443c0f0f8273bf616f909834355
peter.yang <peter.yang@badanamu.com.cn>
2022-02-25 15:16:53 +0800

    fix(CMS-318): correct the invalid sql

--- context.go
+++ context.go
@@ -12,19 +12,30 @@ type DBContext struct {
 
 // Print print sql log
 func (s *DBContext) Printf(format string, v ...interface{}) {
-	if len(v) != 5 {
-		log.Debug(s.DB.Statement.Context, "invalid sql log", log.Any("args", v), log.String("logType", "sql"))
-		return
+	switch len(v) {
+	case 4:
+		// v[4]: [fileWithLineNum(), duration, rowAffected, sql]
+		// example: ["/home/test/code.go:27",16.845856,1,"INSERT INTO `test_table` (`id`,`name`) VALUES ('1234','bad')"]
+		log.Debug(s.Statement.Context, v[3].(string),
+			log.String("logType", "sql"),
+			log.String("lineNum", v[0].(string)),
+			log.Any("rowsAffected", v[2]),
+			log.Float64("duration", v[1].(float64)))
+	case 5:
+		// v[5]: [fileWithLineNum(), "SLOW SQL >= 1µs", duration, rowAffected, sql]
+		// example: ["/home/test/code.go:27","SLOW SQL >= 1µs",16.845856,1,"INSERT INTO `test_table` (`id`,`name`) VALUES ('1234','bad')"]
+		log.Debug(s.Statement.Context, v[4].(string),
+			log.String("logType", "sql"),
+			log.String("lineNum", v[0].(string)),
+			log.Any("rowsAffected", v[3]),
+			log.Float64("duration", v[2].(float64)),
+			log.Any("extra", v[1]))
+	default:
+		log.Debug(s.DB.Statement.Context, "invalid sql log",
+			log.String("logType", "sql"),
+			log.String("format", format),
+			log.Any("args", v))
 	}
-
-	// v[5]: [fileWithLineNum(), "SLOW SQL >= 1µs, duration, rowAffected, sql]
-	// example: ["/home/test/code.go:27","SLOW SQL >= 1µs",16.845856,1,"INSERT INTO `test_table` (`id`,`name`) VALUES ('1234','bad')"]
-	log.Debug(s.Statement.Context, v[4].(string),
-		log.String("logType", "sql"),
-		log.String("lineNum", v[0].(string)),
-		log.Any("rowsAffected", v[3]),
-		log.Float64("duration", v[2].(float64)),
-		log.Any("extra", v[1]))
 }
 
 // GetTableName get database table name of value
